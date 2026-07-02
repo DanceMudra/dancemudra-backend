f5652b80441ea2c5a1496b4e610015c66a7112a2
DanceMudra <info.dancemudra@gmail.com>
2026-07-02 21:12:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,15 +9,9 @@ app.use(express.json());
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
-  host: 'smtp.gmail.com',
-  port: 465, // Yeh secure direct SSL port hai, free servers par best chalta hai
-  secure: true, // Ise true rakho kyunki 465 SSL use karta hai
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false // Isse connection strict security restrictions ki wajah se drop nahi hoga
   }
 });
 
