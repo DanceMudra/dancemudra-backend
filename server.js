926975fb6b9f832a234eda9d8e2d11bcbfb829d8
DanceMudra <info.dancemudra@gmail.com>
2026-07-02 21:21:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: 'info.dancemudra@gmail.com',
+    pass: 'tmuzafzvrahwhuee'
   }
 });
 
